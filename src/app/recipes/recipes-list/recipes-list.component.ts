1b0823eee1df281987c780c34e499a3fd6194418
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-07-04 13:47:29 -0500

    3 files modified

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('First Recipe', 'Just some random recipe', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg'),
+    new Recipe('First Recipe', 'Some random recipe', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg'),
     new Recipe('Second Recipe', 'Another simple recipe', 'https://cdn.pixabay.com/photo/2017/09/17/23/21/gastronomy-2760200_960_720.jpg')
   ];
 
