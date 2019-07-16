1dea648058a1d64242e1d775232bf24a8565d30e
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-07-15 19:56:26 -0500

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,12 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
-import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HeaderComponent, ShoppingListComponent, ShoppingEditComponent, RecipesComponent, RecipesListComponent, RecipeDetailComponent, RecipeItemComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [RecipeService, ShoppingListService]
+  providers: [ShoppingListService]
 })
 export class AppModule { }
